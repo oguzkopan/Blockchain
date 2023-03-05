90901a0bf403e43b419a17941586b8618bcbdfd8
oguzkopan <oguzkopan@gmail.com>
2023-03-05 14:28:28 +0300

    test files

--- backend/tests/blockchain/util/test_crypto_hash.py
+++
@@ -1,5 +0,0 @@
-from backend.util.crypto_hash import crypto_hash
-
-def tes_crypto_hash():
-    assert crypto_hash(1, [2], 'three') == crypto_hash('three', [2], 1)
-    assert crypto_hash('foo') == 'asgadh'
