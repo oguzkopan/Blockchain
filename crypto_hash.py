3144189e186bdab8e84790d5ee6e681c81c087b4
oguzkopan <oguzkopan@gmail.com>
2023-03-05 11:17:52 +0300

    stringified args is added

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,16 +1,17 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def crypto_hash(*args):
     """
     Returns a sha-256 hash of given data
     """
-    stringified_data = json.dumps(data)
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = '|'.join(stringified_args)
 
-    return hashlib.sha256(stringified_data.encode('utf8')).hexdigest()
+    return hashlib.sha256(joined_data.encode('utf8')).hexdigest()
 
 def main():
-    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+    print(f"crypto_hash('foo', 'one', [1]): {crypto_hash('foo', 'one', [1])}")
 
 if __name__ == '__main__':
     main()
