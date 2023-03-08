15471d401cca83f70094587438d2fb10b70f2cb4
oguzkopan <oguzkopan@gmail.com>
2023-03-08 23:25:10 +0300

    hex_to_binary conversion and test functions

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,6 @@
 import time
 from backend.util.crypto_hash import crypto_hash
+from backend.util.hex_to_binary import hex_to_binary
 from backend.config import MINE_RATE
 
 GENESIS_DATA = {
@@ -48,9 +49,10 @@ class Block:
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-        while hash[0:difficulty] != '0' * difficulty:
-            nonce +=1
+        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty:
+            nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -76,7 +78,7 @@ class Block:
         if(new_timestamp - last_block.timestamp) < MINE_RATE:
             return last_block.difficulty + 1
 
-        if(last_block.difficulty -1) > 0:   
+        if(last_block.difficulty - 1) > 0:   
             return last_block.difficulty - 1
         
         return 1
