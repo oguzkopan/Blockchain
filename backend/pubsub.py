97272a7bf44622680aec08365fe271ac968355c4
oguzkopan <oguzkopan@gmail.com>
2023-03-10 14:24:57 +0300

    Pubsub Class and Peer Instances

---
+++ backend/pubsub.py
@@ -0,0 +1,46 @@
+import time
+
+from pubnub.pubnub import PubNub
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.callbacks import SubscribeCallback
+
+from backend.blockchain.block import Block
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = 'sub-c-666638f6-ec63-11e9-b715-9abbdb5d0da2'
+pnconfig.publish_key = 'pub-c-82bf7695-ce5e-4bc5-9cd9-a8f8147dc2a8'
+
+CHANNELS = {
+    'TEST': 'TEST',
+    'BLOCK': 'BLOCK'
+}
+
+class Listener(SubscribeCallback):
+    def message(self, pubnub, message_object):
+        print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
+
+class PubSub():
+    """
+    Handles the publish/subscribe layer of the application.
+    Provides communication between the nodes of the blockchain network.
+    """
+    def __init__(self):
+        self.pubnub = PubNub(pnconfig)
+        self.pubnub.subscribe().channels(CHANNELS['TEST']).execute()
+        self.pubnub.add_listener(Listener())
+
+    def publish(self, channel, message):
+        """
+        Publish the message object to the channel.
+        """
+        self.pubnub.publish().channel(channel).message(message).sync()
+
+def main():
+    pubsub = PubSub()
+
+    time.sleep(1)
+
+    pubsub.publish(CHANNELS['TEST'], { 'foo': 'bar' })
+
+if __name__ == '__main__':
+    main()
