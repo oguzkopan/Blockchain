5ff9bd5fe7d9ab0023380e63dd9a1b76ee4ee2bd
oguzkopan <oguzkopan@gmail.com>
2023-03-11 15:23:27 +0300

    sign data

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,10 +1,18 @@
 import uuid
 
+import json
+
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 class Wallet:
+    """
+    An individual wallet for a miner.
+    Keeps track of the miner's balance.
+    Allows a miner to authorize transactions.
+    """
     def __init__(self):
         self.address = str(uuid.uuid4())[0:8]
         self.balance = STARTING_BALANCE
@@ -14,9 +22,22 @@ class Wallet:
         )
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(
+            json.dumps(data).encode('utf-8'),
+            ec.ECDSA(hashes.SHA256())
+    )
+
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+
+    data =  {'foo': 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
 
 if __name__ == '__main__':
     main()
