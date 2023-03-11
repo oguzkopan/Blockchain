5b255225d6d4b5f8d359b1c0557ea9610ea67f87
oguzkopan <oguzkopan@gmail.com>
2023-03-11 15:31:05 +0300

    verify signatures

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -6,6 +6,7 @@ from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 
 class Wallet:
     """
@@ -31,6 +32,21 @@ class Wallet:
             ec.ECDSA(hashes.SHA256())
     )
 
+    @staticmethod
+    def verify(public_key, data, signature):
+        """
+        Verify a signature based on the original public key and data.
+        """
+        try:
+            public_key.verify(
+                signature,
+                json.dumps(data).encode('utf-8'),
+                ec.ECDSA(hashes.SHA256())    
+            )
+            return True
+        except InvalidSignature:
+            return False
+
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
@@ -38,6 +54,12 @@ def main():
     data =  {'foo': 'bar'}
     signature = wallet.sign(data)
     print(f'signature: {signature}')
+
+    should_be_valid = wallet.verify(wallet.public_key, data, signature)
+    print(f'should_be_valid: {should_be_valid}')
+
+    should_be_invalid = wallet.verify(Wallet().public_key, data, signature)
+    print(f'should_be_invalid: {should_be_invalid}')
 
 if __name__ == '__main__':
     main()
